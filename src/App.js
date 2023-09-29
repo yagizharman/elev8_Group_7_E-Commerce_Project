ed7709c39e82c4cafbc9ddec311f8051be64aa55
Borahan Serel <boraserel@gmail.com>
2023-09-29 17:18:58 +0300

    router setup fixed, redux functions added, cartItem-cart.js updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import Home from './pages/Home';
-import Header from './components/Header';
+import React from "react";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Product from "./components/Product";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <ScrollRestoration />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className='font-bodyFont'>
-      <Header />
-     <Home/>
-      <Footer />
+    <div className="font-bodyFont">
+      <RouterProvider router={router} />
     </div>
   );
 }
