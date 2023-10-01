85174a1813bd14d34dc26368294b6d8d858f4b47
mhozalp <ozalpmervehazal@gmail.com>
2023-10-01 15:22:29 +0300

    small fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
-
+import { persistor, store } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import App from "./App";
