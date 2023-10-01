a948bef5fb5adfd7675ebe5b944ca14dfb3b14ab
mhozalp <ozalpmervehazal@gmail.com>
2023-10-01 16:36:09 +0300

    bug fixed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center gap-10 py-20">
         <div className="w-full flex items-center justify-center gap-10">
-            <div onClick={handleGoogleLogin} className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300">
+            <div className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300">
                 <img className="w-8" src={googleLogo} alt="googleLogo" />
                 <span className="text-sm text-gray-900"> Sign in with Google</span>
             </div>
