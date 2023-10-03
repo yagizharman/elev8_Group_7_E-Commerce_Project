58790d25278b2ccf41123e5f7ba2f52122bbe122
Rashid <rashidgaitov@gmail.com>
2023-10-03 13:56:17 +0300

    Changed api

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function productsData() {
   const products = await axios.get(
-    "https://fakestoreapiserver.reactbd.com/products"
+    "https://fakestoreapiserver.reactbd.com/walmart"
   );
   /*.then(function (response) {
       // handle success
