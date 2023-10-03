85b7b5b0aad08ab588fe2b25abcdec42f2616ca1
EsrefcanTuran <turan.esrefcan@gmail.com>
2023-10-03 11:00:14 +0300

    Make Products Responsive

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import ProductCard from './ProductCard';
+import React from "react";
+import ProductCard from "./ProductCard";
 
-const Products = ({products}) => {
+const Products = ({ products }) => {
   return (
     <div className="py-10">
-       <div className="flex flex-col items-center gap-4">
-        <h1 className="text-2xl bg-black text-white py-2 w-80 text-center"> What is Store? </h1>
-        <span className="w-20 h-[3px] bg-black">  </span>
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl bg-black text-white py-2 w-80 text-center">
+          {" "}
+          What is Store?{" "}
+        </h1>
+        <span className="w-20 h-[3px] bg-black"> </span>
         <p className="max-w-[700px] text-gray-600 text-center">
-        Store is a huge online market place where individuals can buy, sell, trade, 
-        and collect one-of-a-kind products. Additionally, as a community, 
-        we work to promote improvements for people, the environment, and small businesses.
+          Store is a huge online market place where individuals can buy, sell,
+          trade, and collect one-of-a-kind products. Additionally, as a
+          community, we work to promote improvements for people, the
+          environment, and small businesses.
         </p>
-       </div>
-       <div className="max-w-screen-xl mx-auto py-10 grid grid-cols-4 gap-10">
+      </div>
+      <div className="max-w-screen-xl mx-auto py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
         {/* TODO Implement this logic of productscard*/}
-        {/*<ProductCard/> */} 
-        {
-          products.map((item) => (
-            <ProductCard key={item._id} product={item} />
-          ))
-        }
-       </div>
+        {/*<ProductCard/> */}
+        {products.map((item) => (
+          <ProductCard key={item._id} product={item} />
+        ))}
+      </div>
     </div>
-  )
+  );
 };
 
 export default Products;
