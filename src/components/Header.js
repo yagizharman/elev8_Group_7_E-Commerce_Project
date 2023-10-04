907b9bc760ce0f0d0f08b7f06ae608d5f87fe1cf
EsrefcanTuran <turan.esrefcan@gmail.com>
2023-10-04 13:16:46 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,9 @@ const Header = () => {
     <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-800 font-titleFont sticky top-0 z-50">
       <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
         <div>
-          <img src={logoDark} alt="logoDark" className="w-28" />
+          <Link to="/">
+            <img src={logoDark} alt="logoDark" className="w-28" />
+          </Link>
         </div>
         <div className="flex items-center gap-8">
           <ul className="flex items-center gap-8">
@@ -24,34 +26,6 @@ const Header = () => {
                 Home
               </li>
             </Link>
-            {/*<li
-              className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2
-                        decoration-[1px] cursor-pointer duration-300
-                        "
-            >
-              Pages
-            </li>
-            <li
-              className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2
-                        decoration-[1px] cursor-pointer duration-300
-                        "
-            >
-              Shop
-            </li>
-            <li
-              className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2
-                        decoration-[1px] cursor-pointer duration-300
-                        "
-            >
-              Element
-            </li>
-            <li
-              className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2
-                        decoration-[1px] cursor-pointer duration-300
-                        "
-            >
-              Blog
-            </li>*/}
           </ul>
 
           <Link to="/cart">
@@ -65,15 +39,19 @@ const Header = () => {
           <Link to="/login">
             <img
               src={
-                userInfo ? userInfo.image : "https://images.pexels.com/photos/264547/pexels-photo-264547.jpeg?auto=compress&cs=tingsrgb&w=1260&h=750&dpr=1"
+                userInfo
+                  ? userInfo.image
+                  : "https://images.pexels.com/photos/264547/pexels-photo-264547.jpeg?auto=compress&cs=tingsrgb&w=1260&h=750&dpr=1"
               }
               alt="userLogo"
               className="w-8 h-8 rounded-full "
             />
           </Link>
-          {
-            userInfo && <p className="text-base font-titleFont font-semibold underline underline-offset-2" >{userInfo.name}</p>
-          }
+          {userInfo && (
+            <p className="text-base font-titleFont font-semibold underline underline-offset-2">
+              {userInfo.name}
+            </p>
+          )}
         </div>
       </div>
     </div>
